e6996fb3835283a69395ec3cfb7ea58de7acc36b
Dan Nowacki <dan@nowacki.org>
2021-01-24 21:40:25 -0800

    better error handling

--- rds.py
+++ rds.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
 
 import pandas as pd
+import sqlalchemy
 import telnetlib
 
 now = pd.Timestamp.now(tz="US/Pacific")  # .tz_convert('utc')
 print(now)
 
-df = pd.read_sql_query(
-    f"select * from shows where start < datetime('{now}') and end > datetime('{now}') order by start",
-    "sqlite:///../twoweekarchive/shows.db",
-)
+try:
+    df = pd.read_sql_query(
+        f"select * from shows where start < datetime('{now}') and end > datetime('{now}') order by start",
+        "sqlite:///../twoweekarchive/shows.db",
+    )
+    if len(df) > 0:
+        dpsstr = f"{df['title'][0]} on KSQD 90.7 and ksqd.org"
+    else:
+        print("sql query did not receive valid results; falling back to default")
+        dpsstr = f"KSQD 90.7 and ksqd.org"
+except sqlalchemy.exc.OperationalError:
+    print("invalid sql query; falling back to default")
+    dpsstr = f"KSQD 90.7 and ksqd.org"
 
-dpsstr = f"{df['title'][0]} on KSQD 90.7 and ksqd.org"
 print(dpsstr)
 
-tn = telnetlib.Telnet("192.168.0.248", port="10001", timeout=20)
+tn = telnetlib.Telnet("192.168.0.248", port=10001, timeout=10)
 tn.write(f"DPS={dpsstr}\r\n".encode("utf-8"))
 tn.close()
